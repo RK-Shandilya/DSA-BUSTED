8200021eb97528e90ea8090646aa1518ead58769
RK-Shandilya <89962501+RK-Shandilya@users.noreply.github.com>
2022-05-23 08:53:51 +0530

    Create ReverseTheArray.cpp
    Reverse The Array - Two Pointer Approach

---
+++ ReverseTheArray.cpp
@@ -0,0 +1,16 @@
+string reverseWord(string str){
+    
+  //Your code here
+  
+  int i= 0;
+  int j=str.length()-1;
+  
+  while(i<=j)
+  {
+      swap(str[i],str[j]);
+      i++;
+      j--;
+  }
+  return str;
+  
+}
